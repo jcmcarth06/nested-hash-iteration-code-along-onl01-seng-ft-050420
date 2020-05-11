b5dda7508662782140b8c11cb1e6ceba35bc771a
Joshua McArthur <jcmcarth06@gmail.com>
2020-05-11 23:35:06 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,14 +1,4 @@
-require 'pry'
- contacts.each do |person, contact_details_hash|
-   if person == "Freddy Mercury"
-     contact_details_hash.each do |attribute, data|
-       if attribute == :favorite_ice_cream_flavors
-         data.delete_if {|ice_cream == "strawberry"}
-          end
-       end
-     end
-  end
-end
+
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
   #   "Jon Snow" => {
@@ -24,5 +14,13 @@ end
   # }
 
 def remove_strawberry(contacts)
-
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream == "strawberry"}
+           end
+        end
+      end
+   end
 end
